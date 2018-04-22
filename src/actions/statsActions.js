29a49bb0d149eef956052c673bcfa0562234106a
Danilo Rossi <dros@ciklum.com>
2018-04-22 13:52:42 +0200

    Updates

--- src/actions/statsActions.js
+++
@@ -1,9 +0,0 @@
-import * as Action from './types';
-
-export function onGameFinished(twitterUsername, finalScore) {
-  return {
-    type: Action.GAME_FINISHED,
-    twitterUsername,
-    finalScore
-  };
-}
